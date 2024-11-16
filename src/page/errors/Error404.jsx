1b888799b95405418603a4731b05e7747e5407a9
Nguyễn Trung Kiên <truggkien7902@gmail.com>
2024-11-16 17:41:23 +0700

    Update Error404 component to include a link for navigation back to the login page

--- src/page/errors/Error404.jsx
+++ src/page/errors/Error404.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button, Result } from 'antd';
+import { Link } from 'react-router-dom';
 const App = () => (
   <Result
     status="404"
     title="404"
     subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
+    extra={<Button type="primary"><Link to={"/pms/auth/login"}>Back Home</Link></Button>}
   />
 );
 export default App;
