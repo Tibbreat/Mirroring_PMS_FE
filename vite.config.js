77f22862b682fba13b672fa704b128ca37baade7
Nguyễn Trung Kiên <truggkien7902@gmail.com>
2024-11-16 17:29:59 +0700

    Refactor Vite configuration; clean up comments and maintain server settings

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     open: '/pms/auth/login',
-//   },
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Chạy server dev trên cổng 3000
-    open: '/pms/auth/login', // Mở sẵn trang này khi server khởi động
+    port: 3000,
+    open: '/pms/auth/login',
   },
-  build: {
-    outDir: 'dist', // Thư mục đầu ra khi build (mặc định là "dist")
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Tùy chọn gộp chunk (nếu cần)
-      },
-    },
-  },
-  base: '/pms/', // Cấu hình base URL nếu ứng dụng được deploy dưới thư mục con `/pms`
-});
+})
